ca05e27dd06ab787ce41769d02ea6168aed8cbc0
Nyoes <mluusanal@hotmail.com>
2023-03-16 13:36:06 +0300

    Task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+function UTMForm() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [isValidPhone, setIsValidPhone] = useState(false);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const utmSource = localStorage.getItem("utm_source");
+        const utmMedium = localStorage.getItem("utm_medium");
+        const utmTerm = localStorage.getItem("utm_term");
+        const data = {
+            name: name,
+            email: email,
+            phone: phone,
+            utmSource: utmSource,
+            utmMedium: utmMedium,
+            utmTerm: utmTerm,
+        };
+        console.log(data); // Gönderilecek verileri console'da görüntüleyebilirsiniz.Uygulamayı node.js üzerinden başlamış kabul ettim.
+        localStorage.clear(); // localStorage temizleniyor.
+        window.location.href = "/thank-you"; // Teşekkür sayfasına yönlendiriliyor.Teşekkürler sayfası istenmediği için yazma gereği duymadım.
+    };
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    };
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const handlePhoneChange = (event) => {
+        setPhone(event.target.value);
+        // Uluslararası telefon numarası formatını kontrol eden fonksiyon.
+        const regex = /^\+(?:[0-9] ?){6,14}[0-9]$/;
+        setIsValidPhone(regex.test(event.target.value));
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                İsim-Soyisim:
+                <input type="text" value={name} onChange={handleNameChange} />
+            </label>
+            <label>
+                E-posta:
+                <input type="email" value={email} onChange={handleEmailChange} />
+            </label>
+            <label>
+                Telefon:
+                <input type="tel" value={phone} onChange={handlePhoneChange} />
+                {!isValidPhone && <span style={{ color: "red" }}>Geçersiz telefon numarası.</span>}
+            </label>
+            <input type="submit" value="Gönder" />
+        </form>
+    );
 }
 
-export default App;
+export default UTMForm;
